6a14b1a084a4e354840ef90f4ba5c41c41c7a054
jah1603 <hendersonjamesalexander@gmail.com>
2019-03-20 13:22:32 +0000

    Logo fading in from top

--- src/components/slides/intro.js
+++ src/components/slides/intro.js
@@ -18,9 +18,17 @@ export default class Intro extends Component {
 	        live: false
 	    }).init();
 
+			setTimeout(() => {
+				this.writeText("FULL STACK DEVELOPER");
+			}, 1000);
+
 		setTimeout(() => {
-			this.writeText("FULL STACK DEVELOPER");
-		}, 1000);
+			this.setState({
+				android: {height: "50", width: "50"},
+				apple: {height: "50", width: "50"},
+				blog: {height: "50", width: "50"}
+			})
+		}, 100);
 
 		setTimeout(() => {
 			let page = this.props.fullpageApi.getActiveSection();
@@ -45,23 +53,28 @@ export default class Intro extends Component {
 
 	render() {
 		return(
-			<div className="section first">
+			<div style={{height: 450}} className="section first">
 				<div className="text-container wow fadeInDown" data-wow-delay="100ms">
 	        		<h1 style={name}>JAMES<span style={divider}>/</span>HENDERSON</h1>
 	        		<p style={marginTop}>
 	        			{this.state.text}
 	        			<span id="holder"></span><span className="blinking-cursor">|</span>
 	        		</p>
-							<a href="https://play.google.com/store/apps/developer?id=LevelApps+Edinburgh" className="us_link" target="_blank">
-								<img className="contact-icon" src={require("../../assets/android.png")} alt="Android Store Link" width="50" height="50"></img>
+
+							<div className="text-container wow fadeInDown" data-wow-delay="100ms">
+							<a className="text-container wow fadeInDown" data-wow-delay="100ms" href="https://play.google.com/store/apps/developer?id=LevelApps+Edinburgh" className="us_link" target="_blank">
+								<img className="contact-icon wow fadeInDown" data-wow-delay="100ms"  src={require("../../assets/android.png")} alt="Android Store Link" width={this.state.android ? this.state.android.width : 0} height={this.state.android ? this.state.android.width : 0} ></img>
 							</a>
 
-							<a href="https://itunes.apple.com/us/app/n%C3%BCv/id1455339660?ls=1&mt=8" className="us_link" target="_blank">
-								<img className="contact-icon" src={require("../../assets/apple.png")} alt="Apple Store Link" width="50" height="50"></img>
+							<a className="text-container wow fadeInDown" data-wow-delay="100ms" href="https://itunes.apple.com/us/app/n%C3%BCv/id1455339660?ls=1&mt=8" className="us_link" target="_blank">
+								<img className="contact-icon wow fadeInDown" data-wow-delay="100ms" src={require("../../assets/apple.png")} alt="Apple Store Link" width={this.state.android ? this.state.android.width : 0} height={this.state.android ? this.state.android.width : 0}></img>
 							</a>
-							<a href="https://jah1603.github.io" className="us_link" target="_blank">
-								<img className="contact-icon" src={require("../../assets/blog.png")} alt="Blog Link" width="50" height="50"></img>
+
+							<a className="text-container wow fadeInDown" data-wow-delay="100ms" href="https://jah1603.github.io" className="us_link" target="_blank">
+								<img className="contact-icon wow fadeInDown" data-wow-delay="100ms" src={require("../../assets/blog.png")} alt="Blog Link" width={this.state.android ? this.state.android.width : 0} height={this.state.android ? this.state.android.width : 0}></img>
 							</a>
+							</div>
+
 	        	</div>
 	        	<i id="moveDown" className="fa fa-chevron-down fa-3x bounce" onClick={() => this.props.moveDown()}></i>
 	        </div>
