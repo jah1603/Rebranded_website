b4b9287ad8c1c6dd5c35fc98b474f7203cffa9ea
jah1603 <hendersonjamesalexander@gmail.com>
2019-07-06 21:20:09 +0100

    Change background color of projects section

--- src/components/slides/projects.js
+++ src/components/slides/projects.js
@@ -210,7 +210,7 @@ export default class Projects extends Component {
 
     render() {
     	return(
-			<div style={{width: '100%', backgroundColor: '#BCD4E6', border: 'none'}} className="section third">
+			<div style={{width: '100%', backgroundColor: '#f9fcff', border: 'none'}} className="section third">
 
 		                {this.renderProjects(projects)}
 
