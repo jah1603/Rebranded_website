2276d56006458e40c95d86414003dd769e165458
jah1603 <hendersonjamesalexander@gmail.com>
2019-07-29 22:37:11 +0100

    Updates

---
+++ precache-manifest.c41bce56681f48e1e69c8ada6b34b261.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "d014f25ea3b6799086ca",
+    "url": "/Rebranded_website/static/css/main.8264c56a.chunk.css"
+  },
+  {
+    "revision": "d014f25ea3b6799086ca",
+    "url": "/Rebranded_website/static/js/main.d014f25e.chunk.js"
+  },
+  {
+    "revision": "abb26b4425cf6d8921ca",
+    "url": "/Rebranded_website/static/css/1.acfcb195.chunk.css"
+  },
+  {
+    "revision": "abb26b4425cf6d8921ca",
+    "url": "/Rebranded_website/static/js/1.abb26b44.chunk.js"
+  },
+  {
+    "revision": "4a3e57364542ba576075",
+    "url": "/Rebranded_website/static/js/runtime~main.4a3e5736.js"
+  },
+  {
+    "revision": "bd13093f4015bb8b2d99dcea8a300606",
+    "url": "/Rebranded_website/static/media/playstore.bd13093f.png"
+  },
+  {
+    "revision": "d12d096a377889989bdc19283fd4f2a1",
+    "url": "/Rebranded_website/static/media/nuviphone6.d12d096a.png"
+  },
+  {
+    "revision": "a5edbc5525a818a7f0ee283b66a95098",
+    "url": "/Rebranded_website/static/media/findasaur2.a5edbc55.png"
+  },
+  {
+    "revision": "2cce3e7889e668af5e1282a6688c6536",
+    "url": "/Rebranded_website/static/media/wtowalk.2cce3e78.png"
+  },
+  {
+    "revision": "f60891e73e3c5ef6555c70165aa4e38e",
+    "url": "/Rebranded_website/static/media/wtowed.f60891e7.png"
+  },
+  {
+    "revision": "85fd032afdee3ef8538217b43f2af929",
+    "url": "/Rebranded_website/static/media/Zfix.85fd032a.png"
+  },
+  {
+    "revision": "fd690be8d9af13bca6b8f575451374e1",
+    "url": "/Rebranded_website/static/media/InvStory.fd690be8.png"
+  },
+  {
+    "revision": "21a004693d8c55ff67ef0f86a8a4e4f7",
+    "url": "/Rebranded_website/static/media/reachdating.21a00469.png"
+  },
+  {
+    "revision": "c13dd26449a1b5ae76f5f1159df58e87",
+    "url": "/Rebranded_website/static/media/james_site.c13dd264.png"
+  },
+  {
+    "revision": "0954fe80d40b6743447f3d0efc61d435",
+    "url": "/Rebranded_website/static/media/findasaur2.0954fe80.mp4"
+  },
+  {
+    "revision": "a08e075eb001d80c88b9d78849c7c3a8",
+    "url": "/Rebranded_website/static/media/W2BBQ.a08e075e.mp4"
+  },
+  {
+    "revision": "1198af1f40dd04e9cbdd56e0b20d1134",
+    "url": "/Rebranded_website/static/media/nuvphoto.1198af1f.png"
+  },
+  {
+    "revision": "4addd09f22e8e2e0ab7e2a81ad475ae6",
+    "url": "/Rebranded_website/static/media/jicon2.4addd09f.png"
+  },
+  {
+    "revision": "59e042620c86e1d916e0e4331ebb8408",
+    "url": "/Rebranded_website/static/media/appstore.59e04262.png"
+  },
+  {
+    "revision": "ec05963c78c8347ee1db01128d35886c",
+    "url": "/Rebranded_website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e856dee87184ec4564dd219331dc0fd3.js"
+  "/Rebranded_website/precache-manifest.c41bce56681f48e1e69c8ada6b34b261.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Rebranded_website/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
